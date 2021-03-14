70ac8edec3d91acd7afb24714922243eaae77b52
Thej <tkodiyat01@gmail.com>
2021-03-14 15:39:11 -0700

    installed axios, used promis and async methods of dealing wuth the axios responses

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     
-    onSearchSubit(term){
-        console.log(term)
+    onSearchSubmit_1(term){
+        // console.log(term)
+        axios.get('https://api.unsplash.com/search/photos', {
+            headers: {
+                Authorization: 'Client-ID 9wtV3pYgPuNFJMrEzVNVtS2Z3P_W-P-XZn7eDvZiSnU' 
+            },
+            params: {
+                query: term
+            }
+        })
+        .then((response) => {
+            console.log(response.data.results)
+        });
+        // Above is method 1 using the promise to deal with the response
+    }
+
+    // Method 2 of getting response from axios
+    async onSearchSubmit(term){
+        // console.log(term)
+        const response = await axios
+            .get('https://api.unsplash.com/search/photos', {
+                headers: {
+                    Authorization: 'Client-ID 9wtV3pYgPuNFJMrEzVNVtS2Z3P_W-P-XZn7eDvZiSnU' 
+                },
+                params: {
+                    query: term
+                }
+            });
+        
+        console.log(response.data.results)
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onMySubmit={this.onSearchSubit}/>
+                <SearchBar onMySubmit={this.onSearchSubmit}/>
             </div>
         )
     }
